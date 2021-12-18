4319ce119462a415225d5f39192de46cb1cc0b7f
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-18 14:34:38 +0530

    Create load.go

---
+++ installer/pkg/config/v1/load.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package config
+
+import (
+	"io/ioutil"
+
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/yaml"
+)
+
+func Load(fn string) (*Config, error) {
+	fc, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	err = yaml.UnmarshalStrict(fc, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+// LoadMock produces a valid but non-sensical configuration useful for testing
+func LoadMock() *Config {
+	return &Config{
+		Kind:   InstallationFull,
+		Domain: "test.bhojpur.net",
+		Metadata: Metadata{
+			Region: "eu-west1",
+		},
+		Repository: "ap.gcr.io/bhojpur/build",
+		Observability: Observability{
+			LogLevel: "debug",
+		},
+		Database: Database{
+			InCluster: pointer.Bool(true),
+		},
+		ObjectStorage: ObjectStorage{
+			InCluster: pointer.Bool(true),
+		},
+		ContainerRegistry: ContainerRegistry{
+			InCluster: pointer.Bool(true),
+		},
+		Certificate: ObjectRef{
+			Kind: ObjectRefSecret,
+			Name: "https-certs",
+		},
+		Workspace: Workspace{
+			Runtime: WorkspaceRuntime{
+				FSShiftMethod:        FSShiftFuseFS,
+				ContainerDRuntimeDir: "/run/containerd/io.containerd.runtime.v2.task/k8s.io",
+			},
+		},
+	}
+}
