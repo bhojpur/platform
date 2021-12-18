dc2f5a0c3992f2c474c8a8f806c41ca3430b9082
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-18 15:11:26 +0530

    Create constants.go

---
+++ installer/pkg/components/bhojpur/constants.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package bhojpur
+
+const (
+	Component = "bhojpur"
+)
