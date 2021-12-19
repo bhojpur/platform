76caec58d9ff6c06ce8ec6253cc1cba3e33e540d
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-19 07:43:03 +0530

    Create objects.go

---
+++ installer/pkg/components/application/objects.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package workspace
+
+import "github.com/bhojpur/platform/installer/pkg/common"
+
+var Objects = common.CompositeRenderFunc(
+	networkpolicy,
+	podsecuritypolicies,
+	role,
+	rolebinding,
+	common.DefaultServiceAccount(Component),
+)
