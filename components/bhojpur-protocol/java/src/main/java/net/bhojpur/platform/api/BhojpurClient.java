962796c0b158c669ffe0ca769d7f0a826b5cd6d9
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-20 15:23:53 +0530

    Create BhojpurClient.java

---
+++ components/bhojpur-protocol/java/src/main/java/net/bhojpur/platform/api/BhojpurClient.java
@@ -0,0 +1,11 @@
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package net.bhojpur.platform.api;
+
+public interface BhojpurClient {
+    void connect(BhojpurServer server);
+
+    BhojpurServer server();
+}
