5595345fbfe807ec46ff1d032186f89873961793
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-20 16:13:35 +0530

    Create generate-mock.sh

---
+++ components/bhojpur-protocol/go/generate-mock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+# Licensed under the GNU Affero General Public License (AGPL).
+# See License-AGPL.txt in the project root for license information.
+
+set -x
+
+go install github.com/golang/mock/mockgen@v1.6.0
+
+mockgen \
+    -package=protocol \
+    -self_package=github.com/bhojpur/platform/bhojpur-protocol \
+    -source=bhojpur-service.go > mock.go_tmp > mock.go_tmp
+
+mv mock.go_tmp mock.go
+
+leeway run components:update-license-header
