906f6526ae89c0064bfcb7dee1e7f91d6ed2cd99
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-20 11:43:53 +0530

    Create main.go

---
+++ installer/main.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+package main
+
+import "github.com/bhojpur/platform/installer/cmd"
+
+func main() {
+	cmd.Execute()
+}
