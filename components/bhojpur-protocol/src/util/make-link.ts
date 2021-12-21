39eaad359b27c0c8443ed44ff41a405025f36f50
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-21 11:44:41 +0530

    Create make-link.ts

---
+++ components/bhojpur-protocol/src/util/make-link.ts
@@ -0,0 +1,19 @@
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+function isOpenNewTab(event: MouseEvent): boolean {
+    return event.metaKey || (event.ctrlKey);
+}
+
+export function makeLink(node: HTMLElement, url: string, hover: string): void {
+    node.onclick = event => {
+        let target = '_self';
+        if (isOpenNewTab(event)) {
+            target = '_blank';
+        }
+        window.open(url, target);
+    }
+    node.style.cursor = 'pointer';
+    node.title = hover;
+}
