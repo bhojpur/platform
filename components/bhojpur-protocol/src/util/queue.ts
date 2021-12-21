30352a02d0bd10ee515eac3b067f3be6e92b91cf
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-21 12:05:41 +0530

    Create queue.ts

---
+++ components/bhojpur-protocol/src/util/queue.ts
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 Bhojpur Consulting Private Limited, India. All rights reserved.
+// Licensed under the GNU Affero General Public License (AGPL).
+// See License-AGPL.txt in the project root for license information.
+
+import { Deferred } from "./deferred";
+
+/**
+ * Queues asynchronous operations in a synchronous context
+ */
+export class Queue {
+    protected queue: Promise<any> = Promise.resolve();
+
+    enqueue<T>(operation: () => Promise<T>): Promise<T> {
+        const enqueue = new Deferred<T>();
+        this.queue = this.queue.then(async () => {
+            try {
+                const result = await operation();
+                enqueue.resolve(result);
+            } catch (err) {
+                enqueue.reject(err);
+            }
+        });
+        return enqueue.promise;
+    }
+}
