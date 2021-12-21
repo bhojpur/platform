bd64bf7a5928503f620c808483a91fd770672065
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2021-12-21 08:25:28 +0530

    Create generate-config.sh

---
+++ components/bhojpur-protocol/hack/generate-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+go install -u github.com/a-h/generate/...
+
+schema-generate -p protocol ../data/bhojpur-schema.json > ../go/bhojpur-config-types.go
+
+sed -i 's/json:/yaml:/g' ../go/bhojpur-config-types.go
+gofmt -w ../go/bhojpur-config-types.go
+
+leeway run components:update-license-header
